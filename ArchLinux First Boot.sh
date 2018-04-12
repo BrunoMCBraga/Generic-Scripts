d51540ac8516965062dc74585d49a5c31565fb2d
Bruno Braga <bruno.mh.braga@gmail.com>
2018-04-12 18:40:41 +0100

    Create ArchLinux First Boot.sh

---
+++ ArchLinux First Boot.sh
@@ -0,0 +1,14 @@
+#Getting network
+ip link | grep -Po “(?<=[0-9]{1}: )[^:]+(?=:)” | dhcpcd #get network
+
+#Adding new user to sudoers
+pacman -S —noconfirm sudo
+useradd -m -s lbin/bash sicario
+usermod -a -G wheel sicario
+echo “sicario:sicario” | chpasswd
+
+#Installing GUI
+sudo pacman -S —noconfirm xorg-server xorg-xinit
+pacman -S —noconfirm lxde
+echo “exec startlxde” > /root/.xinitrc
+echo “exec startlxde” > /home/sicario/.xinitrc
