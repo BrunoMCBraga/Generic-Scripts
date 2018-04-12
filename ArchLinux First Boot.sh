901f52aa567abb4566fe66328f0f244168ba510c
Bruno Braga <bruno.mh.braga@gmail.com>
2018-04-12 21:10:43 +0100

    Update ArchLinux First Boot.sh

--- ArchLinux First Boot.sh
+++ ArchLinux First Boot.sh
@@ -22,7 +22,6 @@ echo “exec startlxde” > /home/sicario/.xinitrc
 pacman -S —noconfirm chromium
 
 #Wireless
-echo -e "ctrl_interface=/run/wpa_supplicant\nupdate_config=1" > /etc/wpa_supplicant/wpa_supplicant.conf
 echo /etc/wpa_supplicant/wpa_supplicant.conf << EOT
 "ctrl_interface=/run/wpa_supplicant
 update_config=1
