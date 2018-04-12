781b9aa7bacb2b824fedff196bf2c8f372802085
Bruno Braga <bruno.mh.braga@gmail.com>
2018-04-12 19:25:34 +0100

    Update ArchLinux First Boot.sh

--- ArchLinux First Boot.sh
+++ ArchLinux First Boot.sh
@@ -1,5 +1,9 @@
 #Getting network
+systemctl enable dhcpcd@eth0.service
 ip link | grep -Po “(?<=[0-9]{1}: )[^:]+(?=:)” | dhcpcd #just dhcpcd also works
+pacman -S linux-headers # Needed for the next step
+pacman -S --noconfirm broadcom-wl-dkms #may not be necessary
+pacman -S --noconfirm wpa_supplicant #may not be necessary
 
 #Adding new user to sudoers
 pacman -S —noconfirm sudo
