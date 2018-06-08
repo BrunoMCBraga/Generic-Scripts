567ba21fd706978a6c55d13c6f538b49c820113e
Bruno Braga <bruno.mh.braga@gmail.com>
2018-06-08 19:01:51 +0100

    Update ArchLinux Base Installation.sh

--- ArchLinux Base Installation.sh
+++ ArchLinux Base Installation.sh
@@ -1,19 +1,38 @@
+#Partitioning constants
+MAIN_VOLUME="/dev/nvme0n1"
+EFI_PARTITION="/dev/nvme0n1p1"
+EFI_PARTITION_NAME="EFI"
+BOOT_PARTITION="/dev/nvme0n1p2"
+BOOT_PARTITION_NAME="Boot"
+SWAP_PARTITION="/dev/nvme0n1p3"
+SWAP_PARTITION="Swap"
+DATA_PARTITION="/dev/nvme0n1p4"
+DATA_PARTITION_NAME="Data"
+
+
+#Other variables
+HOSTNAME="SICARIO"
+FQHN="SICARIO.NORSE.SEC"
+
+#Accounts and credentials
+ROOT_PASSWORD="toor"
+
 #ArchLinux script
 loadkeys pt-latin9
 timedatectl set-ntp true
 
 #Partitioning volumes
-sgdisk -Z /dev/nvme0n1
-sgdisk -og /dev/nvme0n1
-sgdisk -n 1::+512M -n 2::+200M -n 3::+8G -n 4::  -t 1:ef00 -t 2:8300 -t 3:8200  -t 4:8300 -c 1:”EFI Partition” -c 2:”BOOT Partition” -c 3:”SWAP” -c 4:“”
-mkfs.fat [-s1 -F32] /dev/nvme0n1p1  
-mkfs.ext4 /dev/nvme0n1p2 
-mkswap /dev/nvme0n1p3
-mkfs.ext4 /dev/nvme0n1p4
-swapon /dev/nvme0n1p3
-mount /dev/nvme0n1p4 /mnt
+sgdisk -Z "$MAIN_VOLUME"
+sgdisk -og "$MAIN_VOLUME"
+sgdisk -n 1::+512M -n 2::+200M -n 3::+8G -n 4::  -t 1:ef00 -t 2:8300 -t 3:8200  -t 4:8300 -c 1:"$EFI_PARTITION_NAME" -c 2:"$BOOT_PARTITION_NAME" -c 3:"SWAP_PARTITION_NAME" -c 4:"$DATA_PARTITION_NAME"
+mkfs.fat [-s1 -F32] "$EFI_PARTITION" 
+mkfs.ext4 "$BOOT_PARTITION" 
+mkswap "$SWAP_PARTITION"
+mkfs.ext4 "$DATA_PARTITION"
+swapon "$SWAP_PARTITION"
+mount "$DATA_PARTITION" /mnt
 mkdir /mnt/boot
-mount /dev/nvme0n1p2 /mnt/boot
+mount "$BOOT_PARTITION" /mnt/boot
 
 #Install base packages and generate fstab
 pacstrap /mnt base base-devel
@@ -23,19 +42,19 @@ arch-chroot /mnt
 #Timezone and stuff
 ln -sf /usr/share/zoneinfo/Portugal /etc/localtime
 hwclock --systohc
-sed -i -e “s/#en_US\.UTF-8 UTF-8/en_US\.UTF-8 UTF-8\g” /etc/locale.gen
+sed -i -e "s/#en_US\.UTF-8 UTF-8/en_US\.UTF-8 UTF-8\g" /etc/locale.gen
 locale-gen
 echo "LANG=en_US.UTF-8" > /etc/locale.conf
 echo -e "KEYMAP=pt-latin9\nFONT=\nFONT_MAP=" > /etc/vconsole.conf
-echo "sicario” > /etc/hostname
-echo "127.0.0.1	localhost” >> /etc/hosts
+echo "$HOSTNAME" > /etc/hostname
+echo "127.0.0.1	localhost" >> /etc/hosts
 echo "::1 localhost >> /etc/hosts
-echo "127.0.1.1	sicario.norse.sec sicario” >> /etc/hosts
-echo "root:toor” | chpasswd
+echo "127.0.1.1	$FQHN $HOSTNAME" >> /etc/hosts
+echo "root:$ROOT_PASSWORD" | chpasswd
 
 # Installing Bootloader
 pacman -S —noconfirm grub efibootmgr
 mkdir /boot/efi
-mount /dev/nvme0n1p1 /boot/efi
+mount "$EFI_PARTITION" /boot/efi
 grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id=arch_grub
 grub-mkconfig -o /boot/grub/grub.cfg
