0f9a7ff4fc11f4eb99ba489d35e98337797fd850
Bruno Braga <bruno.mh.braga@gmail.com>
2018-06-12 14:56:43 +0100

    Update iptablesBasic.sh

--- iptablesBasic.sh
+++ iptablesBasic.sh
@@ -24,8 +24,8 @@ ip6tables -A INPUT -i lo -j ACCEPT
 ip6tables -A OUTPUT -o lo -j ACCEPT
 
 # Inbound whitelist
-iptables -A INPUT -p tcp –dport 22 -j ACCEPT
-ip6tables -A INPUT -p tcp –dport 22 -j ACCEPT
+iptables -A INPUT -p tcp --dport 22 -j ACCEPT
+ip6tables -A INPUT -p tcp --dport 22 -j ACCEPT
 
 # Allow established sessions to receive traffic (inbound and outbound)
 iptables -A INPUT -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT
@@ -35,4 +35,6 @@ iptables -A OUTPUT -m conntrack --ctstate NEW,ESTABLISHED,RELATED -j ACCEPT
 ip6tables -A OUTPUT -m conntrack --ctstate NEW,ESTABLISHED,RELATED -j ACCEPT
 
    
-
+# Persisting
+service iptables save
+service ip6tables save
